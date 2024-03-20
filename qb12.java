066a7512b425bba2cd91550a9109eba27e27e564
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-03-20 21:28:35 +0530

    qb12.java
    Declare a class called book having author_name as private data member. Extend book class 
    to have two sub classes called book_publication&paper_publication. Each of these classes 
    have private member called title. Write a complete program to show usage of dynamic 
    method dispatch (dynamic polymorphism) to display book or paper publications of given 
    author. Use command line arguments for inputting data.

--- qb12.java
+++ qb12.java
@@ -1,43 +1,51 @@
-class book {
-    String author_name;
+//QB-12 (Data to get from Command line argument method ) // java Main Ishan ABC Raj PQR
 
-    book(String author_name) {
+class book {
+    private String author_name;
+
+    public book(String author_name) {
         this.author_name = author_name;
     }
 
-    void display(String author_name) {
-        System.out.println("Book Author " + author_name);
+    void display() {
+        System.out.println("Book Author name is" + author_name);
     }
 
 }
 
 class book_publication extends book {
-    String title;
+    private String title;
 
-    book_publication(String author_name, String title) {
+    public book_publication(String author_name, String title) {
         super(author_name);
         this.title = title;
+
+    }
+
+    void display() {
+        System.out.println("Title name is:" + title);
+        super.display();
     }
 }
 
 class paper_publication extends book {
-    String title;
+    private String title;
 
-    paper_publication() {
+    public paper_publication(String author_name, String title) {
+        super(author_name);
         this.title = title;
     }
 
-    void display(String title)) {
-        System.out.println("Paper Publication: " + title);
+    void display() {
+        System.out.println("Title name is:" + title);
+        super.display();
     }
-
 }
 
-    class Main {
+class Main {
     public static void main(String[] args) {
-
-        book b1=new book("John Done")
-        book b2=new paper_publication("Machine Learning")
+        book b1 = new book_publication(args[0], args[1]);
+        book b2 = new paper_publication(args[2], args[3]);
         b1.display();
         b2.display();
     }
