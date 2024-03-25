dafc88798d40cbb8251f727a396a9691a988db7e
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-03-25 10:03:40 +0530

    qb17.java
    The abstract Vegetable class has three subclasses named Potato, Brinjal and Tomato. Write 
    an application that demonstrates how to establish this class hierarchy. Declare one instance 
    variable of type String that indicates the color of a vegetable. Create and display instances of 
    these objects. Override the toString() method of Object to return a string with the name of 
    the vegetable and its color.

---
+++ qb17.java
@@ -0,0 +1,65 @@
+// QB-17
+
+abstract class vegetable {
+    String color;
+
+    // Override
+    public String toString() {
+        return " ";
+    }
+
+}
+
+class potato extends vegetable {
+    String color;
+
+    public potato(String color) {
+        this.color = color;
+    }
+
+    // Override
+    public String toString() {
+        return "potato color is " + color;
+    }
+
+}
+
+class brinjal extends vegetable {
+    String color;
+
+    public brinjal(String color) {
+        this.color = color;
+    }
+
+    // Override
+    public String toString() {
+        return "brinjal color is " + color;
+    }
+
+}
+
+class tomato extends vegetable {
+    String color;
+
+    public tomato(String color) {
+        this.color = color;
+    }
+
+    // Override
+    public String toString() {
+        return "tomato color is " + color;
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        potato p = new potato("Yellow");
+        brinjal b = new brinjal("Violet");
+        tomato t = new tomato("Red");
+
+        System.out.println(p);
+        System.out.println(b);
+        System.out.println(t);
+    }
+}
