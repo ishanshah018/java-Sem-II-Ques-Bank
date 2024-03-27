48129fb733fee0a5c5b392b975c937ce81d09395
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-03-27 23:50:14 +0530

    qb90.java
     handle ArrayIndexOutofBoundsException using try-catch-finally and 
    throw.

---
+++ qb90.java
@@ -0,0 +1,37 @@
+// // By using try..catch..finally
+
+// class ex17 {
+//     public static void main(String[] args) {
+//         String s[] = new String[] { "1", "2", "3" };
+
+//         try {
+//             System.out.println(s[5]);
+//         } catch (ArrayIndexOutOfBoundsException e) {
+//             System.out.println("Not Found");
+//         } finally {
+//             System.out.println("This is finally block..");
+//         }
+
+//     }
+// }
+
+//By using throw 
+
+class ex17 {
+    public static void main(String[] args) {
+        int[] numbers = { 1, 2, 3 };
+        int index = 5;
+
+        try {
+            if (index >= numbers.length) {
+                throw new ArrayIndexOutOfBoundsException("Index " + index + " is out of bounds for the array");
+            }
+
+            System.out.println(numbers[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("This is finally block");
+        }
+    }
+}
