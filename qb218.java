60edbc7b55a3500905ebb7a5a936b7c0a6a34462
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-05-12 10:58:16 +0530

    qb218.java

---
+++ qb218.java
@@ -0,0 +1,36 @@
+/* QB 218:
+Write a Java program to insert the specified element at the specified position in the linked 
+list.
+ */
+
+import java.util.*;
+
+class QB218 {
+    public static void main(String[] args) {
+        ArrayList<Integer> a1 = new ArrayList<>();
+        a1.add(10);
+        a1.add(20);
+        a1.add(30);
+        a1.add(40);
+        a1.add(50);
+
+        System.out.println("Original List" + a1);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Specific position(index) to insert new element: ");
+        int pos = sc.nextInt();
+
+        if (pos >= 0 && pos <= a1.size()) {
+            System.out.println("Enter Element to insert: ");
+            int ele = sc.nextInt();
+            a1.add(pos, ele);
+            System.out.println("Updated list: " + a1);
+        } else {
+            System.out.println("Invalid Index");
+            System.out.println();
+            System.out.println("List not Updated");
+        }
+
+    }
+}
